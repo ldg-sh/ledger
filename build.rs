dc3ea7f3ddef5824b831d086a0613a2a9d09013a
Noah <me@noahdunnagan.com>
2025-08-30 19:09:28 -0400

    Bump proto submodule to latest

--- build.rs
+++ build.rs
@@ -1,4 +1,12 @@
+use tonic_prost_build::configure;
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tonic_prost_build::compile_protos("proto/ledger.proto")?;
+    configure()
+        .compile_protos(
+            &[
+                "proto/auth/auth.proto"
+            ],
+            &["proto"],
+        )?;
     Ok(())
 }
