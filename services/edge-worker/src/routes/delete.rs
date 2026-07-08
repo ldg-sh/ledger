1e6397668bfaaffb2e9fe602b0964ed98beb1ddc
Sam Gordon <sam@thesamgordon.com>
2026-07-07 21:00:19 -0400

    fix: delete download sessions

--- services/edge-worker/src/routes/delete.rs
+++ services/edge-worker/src/routes/delete.rs
@@ -9,12 +9,22 @@ pub async fn handle_delete(mut req: Request, ctx: RouteContext<Arc<AppState>>) -
 
     let payload: DeleteFilesRequest = req.json().await?;
 
-    let _response = state.config.make_internal_request::<_, serde_json::Value>(
+    let response = state.config.make_internal_request::<_, serde_json::Value>(
         "/internal/file/delete",
         &user,
         Method::Delete,
         &payload
     ).await?;
+
+    if response.0 == 200 {
+        let kv = ctx.kv("DOWNLOAD_SESSIONS")?;
+
+        for file_id in payload.file_ids {
+            let file_lookup_key = format!("file_map:{}:{}", user.id, file_id);
+
+            kv.delete(&file_lookup_key).await?;
+        }
+    }
 
     Ok(Response::empty()?.with_status(204))
 }
